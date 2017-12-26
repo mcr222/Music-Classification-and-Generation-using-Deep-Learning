eed475623f4cc1624186457d9991d756778e8b82
Diego <o90G03e1258145>
2017-12-26 02:43:43 +0100

    NN version 2.0. Data is obtained from the root batch folder. So far, with the default parameters and not all the data, the accuracy is 42%.

--- NN.py
+++ NN.py
@@ -1,84 +1,128 @@
 from keras.models import Sequential, Model
-from keras.layers import Dense, Conv1D, MaxPooling1D, Flatten, Embedding, Input
+from keras.layers import Dense, Conv1D, MaxPooling1D, Flatten, Embedding, Input, GlobalMaxPooling1D,Dropout
 from keras.utils import normalize
 from keras import optimizers
 
 import numpy as np
 import os
+import keras
+import sklearn.model_selection
+
 # fix random seed for reproducibility
 np.random.seed(7)
 
 
-def createModel(filters, learning_rate):
+def createModel(filters, learning_rate, dropout):
     '''Creates the NN model. As an input, the NN expects a normalized batch of 10 132300-dimensional
-    This is the Version 1 of the architecture, so it might change.
-    '''
-    #sequence_input = Input(shape=shape, dtype='float32')
-    sequence_input = Input(shape=(10,132300), dtype='float32')
-    #x = Embedding(output_dim=512, input_dim=10000, input_length=1000)(sequence_input)
-    l_cov1 = Conv1D(filters, kernel_size=1, activation='relu', input_shape=(10,132300))(sequence_input)
-    l_pool1 = MaxPooling1D(5)(l_cov1)
-    l_cov2 = Conv1D(filters, kernel_size=1, activation='relu')(l_pool1)
-    l_pool2 = MaxPooling1D(1)(l_cov2)
-    l_cov3 = Conv1D(filters, 1, activation='relu')(l_pool2)
-    l_pool3 = MaxPooling1D(2)(l_cov3)  # global max pooling
-    l_flat = Flatten()(l_pool3)
-    l_dense = Dense(filters, activation='relu')(l_flat)
-    preds = Dense(10, activation='softmax')(l_dense)
+    For now, the following parameters are added to the model.
+    @:param filters: number of filters in all the conv1d layers {50, 300}
+    @:param learning_rate: learning rate of the model {0.0, 1.0}
+    @:param dropout: Dropout value {0.0, 1.0}
+        '''
+    model = Sequential()
+    model.add(Embedding(1,
+                        100,
+                        input_length=132300))
+    model.add(Dropout(dropout))
+    model.add(Conv1D(filters, 5, activation='relu'))
+    model.add(MaxPooling1D(5))
+    model.add(Dropout(dropout))
+    model.add(Conv1D(filters, 5, activation='relu'))
+    model.add(MaxPooling1D(5))
+    model.add(Dropout(dropout))
+    model.add(Conv1D(filters, 5, activation='relu'))
+    model.add(MaxPooling1D(35))
+    model.add(Flatten())
+    model.add(Dense(15))
 
-    model = Model(sequence_input, preds)
+    print(model.summary())
+
 
     sgd = optimizers.SGD(lr=learning_rate, decay=1e-6, momentum=0.9, nesterov=True)
-    model.compile(optimizer=sgd, loss='binary_crossentropy', metrics=['accuracy'])
-    print("model fitting - simplified convolutional neural network")
-    # m.summary()
+    model.compile(optimizer=sgd, loss='categorical_crossentropy', metrics=['accuracy'])
+
     return model
 
-'''
-'''
 def main():
     '''
-    Creates the NN model and train it with the batch data available in the input_training_folder file.
+       Creates the NN model and train it with the batch data available in the input_training_folder file.
     '''
 
-    #Parameters: For the NN model, initially two parameters are going to be considered as a parameter.
+    #Global parameters
+    #Input folder that contains all the subfolders A,B,C...containing the batch files.
+
+    #input_folder = "small_train/small_train/"  #In hops
+    input_folder= "C:\\Users\\Diego\\Google Drive\\KTH\\Scalable Machine learning and deep learning\\project\\output2"
+
+
+    #Parameters: For the NN model, initially three parameters are going to be considered as a parameter.
     # The number of filters and learning rate
     filters = 128
     learning_rate = 0.01
-    m = createModel(filters, learning_rate)
+    dropout = 0.2
+    m = createModel(filters, learning_rate, dropout)
 
-    #Input training Data
-    input_training_folder = "./data/training_data"
-    files = os.listdir(input_training_folder)
-    for file in files:
+    #Split training and testing files
+    all_files = []
+    for folder in os.listdir(input_folder):
+        print(folder)
+        for file in os.listdir(input_folder + "\\" + folder):
+            if file.startswith("songs_"):
+                all_files.append(input_folder + "\\" + folder + "\\" + file)
+
+    res = sklearn.model_selection.train_test_split(all_files, test_size=0.3, shuffle=True)
+    print("Training files: " + str(res[0]))
+    print("Test files: " + str(res[1]))
+
+    #We train the model. For each file in the training folder, we extract the batch, normalize it and then call the function
+    #train on batch from keras.
+    training_files = res[0]
+    for file in training_files:
 
         #Get input and label data from the same batch
-        if file.startswith("songs"):
-            batch = file.replace("songs", "")
+        if "songs_" in file:
+            label = file.replace("songs", "labels")
             print("training " + file)
-            data = np.load(input_training_folder + "\\" + str(file))
-            X = normalizeData(data)
+            data = np.load(str(file))
+            normalizedData = normalize(data, axis=0, order=2)
 
-            labels = np.load(input_training_folder + "\\" + "labels" + str(batch))
-            labels = np.expand_dims(labels, axis=0)
+            labels = np.load(str(label))
+            listY = []
+            for d in labels:
+                listY.append([d])
+            m.train_on_batch(normalizedData, keras.utils.to_categorical(listY, num_classes=15) )
 
-            m.train_on_batch(X, labels )
+    # We test the model. For each file in the test folder, we extract the batch, normalize it and then call the function
+    # test on batch from keras. Note that the test_on_batch function does not add to all batches, but it gives testing metrics
+    #per batch. Thus, it is necessary to add all the results.
+    test_files = res[1]
+    total_loss = 0
+    total_accuracy = 0
+    total_test_data = 0
 
+    for file in test_files:
+        if "songs_" in file :
+            label = file.replace("songs", "labels")
+            print("test " + file)
+            x_test = np.load(file)
+            x_test_normalized = normalize(x_test, axis=0, order=2)
 
-    #testing data
-    # input_test_folder = ".\\data\\test_data"
-    # X = np.load(input_test_folder + "\\songs_10batch1.npy")
-    # xx = normalizeData(X)
-    # Y =np.load(input_test_folder + "\\labels_10batch1.npy")
-    # m.test_on_batch(xx,Y)
+            labels_test = np.load(label)
+            y_test = []
+            for d in labels_test:
+                y_test.append([d])
 
+            x = m.test_on_batch(x_test_normalized, keras.utils.to_categorical(y_test, num_classes=15))
 
-def normalizeData(data):
-    normalizedData = normalize(data, axis=0, order=2)
-    #print(data.shape)
-    resp = np.expand_dims(normalizedData, axis=0)
-    #print(X.shape)
-    return resp
+            print("New batch: Test Loss " + str(x[0]) +  "Test accuracy " + str(x[1]))
+            total_test_data +=1
+            total_loss +=x[0]
+            total_accuracy +=x[1]
 
+    print("[Total test files] " + str(total_test_data))
+    print("[total_loss] " + str(total_loss) + " [total_loss] " + str(total_loss/total_test_data))
+    print("[total_accuracy] " + str(total_accuracy) + " [total_accuracy] " + str(total_accuracy/total_test_data))
+
+    return total_accuracy/total_test_data
 
 main()
